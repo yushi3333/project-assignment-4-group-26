1233764e7994233593ffebeee8a7a71e194c545e
yushi <a1125189291@gmail.com>
2024-11-10 12:48:25 -0500

    create the signup function

--- YuShiGan/react-app/src/App.jsx
+++ YuShiGan/react-app/src/App.jsx
@@ -1,17 +1,31 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Register from './Register/register.jsx'
+import Login from '../src/Login/login.jsx'
+import Home from "../src/Home/home.jsx"
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <main className = "App">
-      <Register />
+    <Router>
+       <main className = "App">
+        <Routes>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/" element={<Register/>}/>
+          <Route path="/home" element={<Home/>}/>
+
+        </Routes>
       
-    </main>
+      
+      </main>
+
+    </Router>
+   
     
   );
 }
